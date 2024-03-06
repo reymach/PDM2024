55404d66205693d894d5dacb8f44eb0db1a3fe47
reymach <reyamachen@gmail.com>
2024-03-05 21:20:08 -0600

    soundlecture04

---
+++ soundlecture04/sketch.js
@@ -0,0 +1,24 @@
+
+let sine = new touchEnded.Synth ({
+  oscillator: {
+    type: 'sine'
+  }
+}).toDestination
+
+let sine = new touchEnded.Synth ({
+  oscillator: {
+    type: 'sine'
+  }
+}).toDestination
+
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  text('Q = Sine', 150, 150)
+  
+}
